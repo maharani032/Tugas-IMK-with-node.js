4c3a9a361a6bb7966d027f26b4b8661c4819b46a
Maharani Nirmala Dewi <contact.maharani.d@gmail.com>
2020-12-03 19:07:14 +0700

    login with google done

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require( "mongoose" );
 const session = require( 'express-session' );
 const passport = require( "passport" );
 const passportLocalMongoose = require( "passport-local-mongoose" );
+const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
+const findOrCreate = require( 'mongoose-findorcreate' );
 const marked = require( 'marked' );
 const createDomPurify = require( 'dompurify' );
 const { JSDOM } = require( 'jsdom' );
@@ -64,12 +66,9 @@ const postSchema = new mongoose.Schema( {
     }
 } );
 const userSchema = new mongoose.Schema( {
-    email: {
-        type: String
-    },
-    password: {
-        type: String
-    }
+    email: String,
+    password: String,
+    googleId: String
 } );
 postSchema.pre( "validate", function ( next ) 
 {
@@ -83,26 +82,56 @@ postSchema.pre( "validate", function ( next )
 // userSchema.plugin( encrypt, { secret: process.env.SECRET, encryptedFields: [ "password" ] } );
 userSchema.plugin( passportLocalMongoose );
 
-// userSchema.plugin( findOrCreate );
+userSchema.plugin( findOrCreate );
 // postSchema.index( { 'content': 'text' } );
 const Post = new mongoose.model( "Post", postSchema );
 const User = new mongoose.model( "User", userSchema );
 
 passport.use( User.createStrategy() );
+
 passport.serializeUser( function ( user, done )
 {
-    done( null, user );
+    done( null, user.id );
 } );
-passport.deserializeUser( function ( user, done )
+
+passport.deserializeUser( function ( id, done )
 {
-    done( null, user );
+    User.findById( id, function ( err, user )
+    {
+        done( err, user );
+    } );
 } );
+passport.use( new GoogleStrategy( {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/refrain",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function ( accessToken, refreshToken, profile, cb )
+    {
+        console.log( profile );
+        User.findOrCreate( { googleId: profile.id }, function ( err, user )
+        {
+            return cb( err, user );
+        } );
+    }
+) );
 //close database
 
 app.get( '/', ( req, res ) =>
 {
     res.render( "main" )
 } );
+app.get( '/auth/google',
+    passport.authenticate( 'google', { scope: [ 'profile' ] } )
+);
+app.get( '/auth/google/refrain',
+    passport.authenticate( 'google', { failureRedirect: '/login' } ),
+    function ( req, res )
+    {
+        // Successful authentication, redirect home.
+        res.redirect( '/Home' );
+    } );
 app.get( "/Comedy", ( req, res ) =>
 {
     Post.find( { genre: "Comedy" }, ( err, posts ) =>
