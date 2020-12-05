4c2a3f83cbacb4ce3069e521e64e267f05196dcb
Maharani Nirmala Dewi <contact.maharani.d@gmail.com>
2020-12-05 19:37:47 +0700

    up

--- app.js
+++ app.js
@@ -535,8 +535,8 @@ app.put( "/profil/edit/:id", async ( req, res, next ) =>
     next()
 }, saveArticleAndRedirect( 'profil' ) )
 
-// let port = process.env.PORT;
-// if ( port == null || port == "" ) {
-//     port = 3000;
-// }
-app.listen( process.env.PORT || 3000 );
+
+app.listen( process.env.PORT || 3000, function ()
+{
+    console.log( "Express server listening on port %d in %s mode", this.address().port, app.settings.env );
+} );
