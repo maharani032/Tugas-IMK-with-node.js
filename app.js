2389aed36a2a7f446b9228ba04b74c4797f517cd
Maharani Nirmala Dewi <contact.maharani.d@gmail.com>
2020-12-05 18:17:57 +0700

    make it bette

--- app.js
+++ app.js
@@ -535,8 +535,8 @@ app.put( "/profil/edit/:id", async ( req, res, next ) =>
     next()
 }, saveArticleAndRedirect( 'profil' ) )
 
-let port = process.env.PORT;
-if ( port == null || port == "" ) {
-    port = 3000;
-}
-app.listen( port, () => console.log( "server up" ) );
+// let port = process.env.PORT;
+// if ( port == null || port == "" ) {
+//     port = 3000;
+// }
+app.listen( process.env.PORT || 3000 );
